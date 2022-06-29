d4684873e8a24fd9350721c3cbb84e2d5bb1e331
Suyeon Park <s2026@e-mirim.hs.kr>
2022-06-29 09:38:24 +0900

    #1 메세지박스, 모달, 모달창 mdi 폼 작성

---
+++ Modalmodel/CustomForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modalmodel
+{
+    public partial class CustomForm : Form
+    {
+        public CustomForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CustomForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Modalmodel/Form1.cs
+++ Modalmodel/Form1.cs
@@ -15,6 +15,47 @@ namespace Modalmodel
         public Form1()
         {
             InitializeComponent();
+            IsMdiContainer = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요", "비 오는 날 아침 인사");
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요", "비 오는 날 아침 인사",
+            MessageBoxButtons.YesNoCancel,MessageBoxIcon.Hand);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요", "비 오는 날 아침 인사",
+            MessageBoxButtons.YesNoCancel, MessageBoxIcon.Hand);
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CustomForm frm = new CustomForm(); 
+            frm.ShowDialog();
+
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            CustomForm frm = new CustomForm();
+            frm.MdiParent = this;
+            frm.Show();
         }
     }
 }
